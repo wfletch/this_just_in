79922c1b4330af6f01965725fb6eeff91afda0f9
Warren Fletcher <warren@fletch.band>
2022-06-20 22:58:03 +0200

    Added More Ideas

--- thisjustin.py
+++ thisjustin.py
@@ -2,3 +2,5 @@
 #   1:  Starts Tracking Time
 #   2:  Autoamtically calculates and labels tasks
 #   3:  Creates weekly reports and sends them to me/Julia
+#   4:  Can add Links To Storage/Things I have created with todays practice
+#   5:  Can have a more intuitive UI. This is going to be fun  
