82c825d1edab277e4d42495c1d5d39ecf79458f8
Warren Fletcher <warren@fletch.band>
2022-06-24 23:42:44 +0200

    WIP: Everything

--- thisjustin.py
+++ thisjustin.py
@@ -3,4 +3,7 @@
 #   2:  Autoamtically calculates and labels tasks
 #   3:  Creates weekly reports and sends them to me/Julia
 #   4:  Can add Links To Storage/Things I have created with todays practice
-#   5:  Can have a more intuitive UI. This is going to be fun  
+#   5:  Can have a more intuitive UI. This is going to be fun
+
+if __name__ == "__main__":
+    print("Hello World, This is going to take some time.")
